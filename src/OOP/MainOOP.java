74a1c2d6d3ac3c975eb41099180546182ff1a657
Paramunk <k.manukyan1992@gmail.com>
2022-06-06 12:17:52 +0400

    new

---
+++ src/OOP/MainOOP.java
@@ -0,0 +1,17 @@
+package OOP;
+
+import java.nio.charset.StandardCharsets;
+
+public class MainOOP {
+    public static void main(String[] args) {
+
+        Human human1 = new Human();
+        human1.firstname = "Karlen";
+        //System.out.println(human1.lastname);
+        //System.out.println(human1.toString());
+        //human1.sayHello();
+        human1.setAge(20);
+        //System.out.println(human1.getAge());
+
+    }
+}
